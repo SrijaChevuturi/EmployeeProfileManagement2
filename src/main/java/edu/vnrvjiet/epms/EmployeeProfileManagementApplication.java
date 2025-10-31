de9148c586465042fb926f5a6506d87a93362d42
Sai <srija.chevuturi@gmail.com>
2025-10-31 11:13:15 +0530

    First Commit

---
+++ src/main/java/edu/vnrvjiet/epms/EmployeeProfileManagementApplication.java
@@ -0,0 +1,14 @@
+package edu.vnrvjiet.epms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProfileManagementApplication {
+
+	public static void main(String[] args) {
+        //sample
+		SpringApplication.run(EmployeeProfileManagementApplication.class, args);
+        System.out.println("Tomcat running for employee profile management service...");
+	}
+}
